3ee659e005428633bcf93d5b03b851a75a2eb5e8
Fabricio Rostand Morais <fabriciorostand2000@gmail.com>
2024-12-28 19:48:58 -0300

    Create path.js and fs.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { Person } = require("./person");
 
+// require("./modules/path");
+// require("./modules/fs");
+
 const person = new Person('Fabricio');
 
 console.log(person.sayMyName());
---
+++ modules/path.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+// Nome do Diretório atual 
+console.log(path.dirname(__filename));
+
+// Nome do arquivo atual
+console.log(path.basename(__filename));
