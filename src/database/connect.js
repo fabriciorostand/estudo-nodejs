f7c90207fb4630bb5533f1f72594e0aed2585b7d
Fabricio Rostand Morais <fabriciorostand2000@gmail.com>
2024-12-29 19:57:53 -0300

    Create express.js, connect.js and user.model.js

---
+++ src/database/connect.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.tuc1m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+        console.log('Conexão ao banco de dados realizada com sucesso!');
+    } catch (error) {
+        console.log('Ocorreu um erro ao tentar se conectar com o banco de dados: ', error);
+    }
+}
+
+module.exports = connectToDatabase;
