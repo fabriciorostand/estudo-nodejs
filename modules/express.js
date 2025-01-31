b263003269190f12c1c1b9a167cdb9d547b48a39
Fabricio Rostand Morais <fabriciorostand2000@gmail.com>
2025-01-31 09:11:29 -0300

    fix: ensure CORS configuration is applied correctly

--- modules/express.js
+++ modules/express.js
@@ -16,6 +16,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(express.json());
 
+// Configuração do CORS
 const corsOptions = {
     origin: 'https://fabriciorostand.github.io',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
@@ -23,6 +24,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+// Middleware de Logging
 app.use((req, res, next) => {
     console.log(`Request Type: ${req.method}`);
     console.log(`Content Type: ${req.headers["content-type"]}`);
@@ -31,10 +33,10 @@ app.use((req, res, next) => {
     next();
 });
 
+// Rotas
 app.get('/users', async (req, res) => {
     try {
         const users = await UserModel.find({});
-
         res.status(200).json(users);
     } catch (error) {
         return res.status(500).send(error.message);
@@ -46,23 +48,21 @@ app.post('/login', login);
 app.get('/users/:id', async (req, res) => {
     try {
         const id = req.params.id;
-
         const user = await UserModel.findById(id);
-
         return res.status(200).json(user);
     } catch (error) {
         return res.status(500).send(error.message);
     }
-})
+});
 
 app.post('/users', async (req, res) => {
     try {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        
+
         if (!emailRegex.test(req.body.email)) {
             return res.status(400).json({ message: 'O e-mail fornecido é inválido.' });
         }
-        
+
         const existingUser = await UserModel.findOne({ email: req.body.email });
         if (existingUser) {
             return res.status(409).json({ message: 'Este e-mail já foi usado para cadastrar uma conta.' });
@@ -78,26 +78,22 @@ app.post('/users', async (req, res) => {
 app.patch('/users/:id', async (req, res) => {
     try {
         const id = req.params.id;
-        
         const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
-
         res.status(200).json(user);
     } catch (error) {
         res.status(500).send(error.message);
     }
-})
+});
 
 app.delete('/users/:id', async (req, res) => {
     try {
         const id = req.params.id;
-        
         const user = await UserModel.findByIdAndDelete(id);
-
         res.status(200).json(user);
     } catch (error) {
         res.status(500).send(error.message);
     }
-});  
+});
 
 const port = process.env.PORT || 8080;
 
